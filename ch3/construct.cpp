11cd0e247affbf188fb1c784e14cb6c9ee617bf5
stephan14 <zsf657135507@gmail.com>
2018-03-01 18:12:10 +0800

    C++复制初始化优化问题

---
+++ ch3/construct.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class	A
+{
+	public:
+		A(int i = 0):val(i){}
+        //复制初始化首先使用指定构造函数创建一个临时对象，然后用复制构造函数将那个临时对象复制到正在创建的对象。但是 C++ 标准允许编译器在某些特定情况下把复制构造的步骤省略掉，这样就可以节省若干临时变量,可以用 -fno-elide-constructors 把这一优化关闭
+		A(const A& a):val(a.val){
+			cout << "come to copy create" << endl;
+		}
+	private:
+		int val;
+};
+
+int main()
+{
+	A a = 3;
+	return 0;
+}
