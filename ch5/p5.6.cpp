6b52de3c97932611ac3e844f3c6a9124df8f5f1c
zoushengfu <zoushengfu@bytedance.com>
2018-08-16 13:43:09 +0800

    修改bug

--- ch5/p5.6.cpp
+++ ch5/p5.6.cpp
@@ -1,15 +1,14 @@
-#include <iostram>
+#include <iostream>
 #include <vector>
 #include <string>
 
 int main() {
-  std::vector<string> score = {"F", "D", "C", "B", "A", "A++"};
+  std::vector<std::string> score = {"F", "D", "C", "B", "A", "A++"};
 
-  for (int g; cin >> g) {
+  for (int g; std::cin >> g;) {
     std::string letter = g < 60 ? score[0] : score[(g - 50)/ 10];
     letter += g % 10 > 7? "+" : g % 10 < 3? "-" : "";
+    std::cout << letter << std::endl;
   }
-
-  std::cout << letter << std::endl;
   return 0;
 }
