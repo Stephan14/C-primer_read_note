467d57491af8f243ff99aeea47ec86aa31c5ea80
zoushengfu <zoushengfu@bytedance.com>
2019-01-05 19:47:19 +0800

    构造函数

--- ch7/p7.5.cpp
+++ ch7/p7.5.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 
-class Person {
+struct Person {
 
-    std::string name;
+    std::string name = "name";
     std::string address;
 
 public:
@@ -14,13 +14,19 @@ public:
         this->address = address;
     }
 
+    Person() {
+        //name = "test1";
+    }
+
     auto getName() const -> std::string const& { return name; } //auto 类型推倒时会忽略掉引用类型
     //decltype(auto) getName() const -> std::string const& { return name; } //auto 类型推倒时会忽略掉引用类型.c++14
     auto getAddress() const -> const std::string&  { return address; }
 };
 
 int main() {
-    Person p("name", "beijing");
+    Person p("name_1", "beijing");
+    Person t;
     std::cout << p.getName();
+    std::cout << t.getName();
     return 0;
 }
