06a899c3927ab63f6ee3b6f83317f5ca29e79b25
zoushengfu <zoushengfu@bytedance.com>
2019-04-01 10:27:59 +0800

    自定义析构函数

---
+++ ch12/p12.15.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <memory>
+
+struct destintaion {
+    std::string ip;
+    std::string port;
+    destintaion(std::string ip, std::string port):port(port), ip(ip){}
+};
+
+struct connection {
+    std::string ip;
+    std::string port;
+    connection(std::string ip, std::string port):port(port), ip(ip) {}
+};
+
+connection conn(destintaion *dest) {
+    std::shared_ptr<connection> p(new connection(dest->ip, dest->port));
+    std::cout << "connect to " << p->ip;
+    return *p;
+}
+
+void disconnect(connection *con) {
+    std::cout << "disconnect " << con->ip;
+}
+
+void end_connect(connection *con) {
+    disconnect(con);
+}
+
+void f(destintaion &d)
+{
+	connection conn = conn(&d);
+	std::shared_ptr<connection> p(&conn, [](connection *con) { disconnect(con);  });
+	std::cout << "connecting now(" << p.use_count() << ")" << std::endl;
+}
